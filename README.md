ab31613acc2a9c7cf95d2e46eb1660bb5eed0d74
Stuart <stuartsindani06@gmail.com>
2026-04-29 10:30:04 +0200

    about to push first login page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WEBD-2200-FinalProject
 CSS Group 3 final project for Web Development module.
+29/04/2026
+stuart--------------
+Added login page where we are assuming that all users will login through.Another assumption is that users will use their hospital provided emails to login.
+
--- login.php
+++ login.php
@@ -9,18 +9,18 @@
 <body>
     <div id="page">
         <div id="head_container">
-            <img alt="Hopspital animation" src="images/hospital_svgrepo_com.svg">
-            <h1>NTHANZI HOSPITAL</h1>
+            <img alt="Hopspital animation" src="images\logo.svg">
+            <h1>MEDICARE</h1>
             <h3>Treating our patients with love and care</h3>
         </div>
         <div id="form_container">
             <form method="POST">
                 
-                    <!-- <legend></legend> -->
+                   
                     <label for="userid">Email</label><br>
-                    <input id="userid" type="text" name="userid" placeholder="example@nthanzi.mw" pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" required><br> <!--yet to add pattern that only allows sub domain email adresses-->
+                    <input id="userid" type="email" name="userid" title="Please ensure your email contains the company domain '@nthanzi.mw'" placeholder="example@nthanzi.mw" required><br> <!--validation not yet set to regex.pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" might not use it-->
                     <label for="passcode">password</label><br>
-                    <input type="password" id="passcode" name="passcode" required><br><br>
+                    <input type="password" id="passcode" name="passcode" required minlength="12"><br><br>
                     <button type="submit" id="submit" name="submit">sign in</button>
                 
             </form>
