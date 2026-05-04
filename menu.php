f134055b9527f35807f4be94184886e67f7264da
precious <pmaway5@gmail.com>
2026-05-04 21:49:14 +0200

    Implemented dynamic dashboard with role based navigation, stts n logout functionality

--- menu.php
+++ menu.php
@@ -1,51 +1,71 @@
 <?php
 
+$role = isset($_SESSION['role']) ? $_SESSION['role'] : '';
+
 echo '
+<li>
+    <div class="icon">
+        <img src="images/dashboard.svg" alt="dashboard icon">
+    </div>
+    <a href="dashboard.php">Dashboard</a>
+</li>';
 
-                    <li>
-                        <div class="icon">
-                            <img src="images/dashboard.svg" alt="dashboard icon">
-                        </div>
-                        <a href="dashboard.php">Dashboard</a>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="images/doctors.svg" alt="doctors icon icon">
-                        </div>
-                        <a href="doctors.php">Doctors</a>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="images/patients3.svg" alt="patients icon">
-                        </div>
-                        <a href="patients.php">Patients</a>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="images/records.svg" alt="medical_records icon">
-                        </div>
-                        <a href="medical_records.php">Medical Records</a>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="images/department.svg" alt="departments icon">
-                        </div>
-                        <a href="departments.php">Departments</a>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="images/billing2.svg" alt="patients icon">
-                        </div>
-                        <a href="billing.php">Billing</a>
-                    </li>
-                    <li>
-                        <div class="icon">
-                            <img src="images/reports.svg" alt="patients icon">
-                        </div>
-                        <a href="reports.php">Reports</a>
-                    </li>
+if ($role === 'Doctor') {
+    echo '
+    <li>
+        <div class="icon">
+            <img src="images/doctors.svg" alt="doctors icon">
+        </div>
+        <a href="doctors.php">Doctors</a>
+    </li>';
+}
 
-';
+echo '
+<li>
+    <div class="icon">
+        <img src="images/patients3.svg" alt="patients icon">
+    </div>
+    <a href="patients.php">Patients</a>
+</li>';
 
+if ($role === 'Doctor' || $role === 'Nurse') {
+    echo '
+    <li>
+        <div class="icon">
+            <img src="images/records.svg" alt="medical records icon">
+        </div>
+        <a href="medical_records.php">Medical Records</a>
+    </li>';
+}
+
+if ($role === 'Doctor') {
+    echo '
+    <li>
+        <div class="icon">
+            <img src="images/department.svg" alt="departments icon">
+        </div>
+        <a href="departments.php">Departments</a>
+    </li>';
+}
+
+if ($role === 'Doctor' || $role === 'Receptionist') {
+    echo '
+    <li>
+        <div class="icon">
+            <img src="images/billing2.svg" alt="billing icon">
+        </div>
+        <a href="billing.php">Billing</a>
+    </li>';
+}
+
+if ($role === 'Doctor') {
+    echo '
+    <li>
+        <div class="icon">
+            <img src="images/reports.svg" alt="reports icon">
+        </div>
+        <a href="reports.php">Reports</a>
+    </li>';
+}
 
 ?>
