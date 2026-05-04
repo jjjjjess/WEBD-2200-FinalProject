f1871a2926f5cfa4d25897cdd516f2f3b0354960
Stuart <stuartsindani06@gmail.com>
2026-05-04 17:12:12 +0200

    adding authentication based on email and password

--- login.php
+++ login.php
@@ -1,3 +1,57 @@
+<?php
+require("db_connect.php");
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $userid = $_POST['userid'] ?? '';
+        $passcode =$_POST['passcode'] ?? '';
+        $error=" ";
+
+        $sql = "SELECT * FROM users WHERE email ='$userid'";
+        $result = mysqli_query($conn, $sql);
+        //if any user matches it should then check from the row if the passwords match aswell.
+        // second if statement checks if the user has entered a valid password.git 
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            if ($row['password'] == $passcode) 
+            {
+                $_SESSION['user_id'] = $row['email'];
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['role'] = $row['role'];
+                // if ($row['role'] == "Admin") 
+                // {
+                //     header("Location: admin_test.php");
+                //     exit;
+                // } 
+                // elseif ($row['role'] == "Doctor") 
+                // {
+                //     header("Location: doctor_test.php");
+                //     exit;
+                // } 
+                // elseif ($row['role'] == "Nurse") {
+                //     header("Location: nurse_test.php");
+                //     exit;
+                // }
+                // elseif($row['role']== "Receptionist")
+                // {
+                //     header("Location:");
+                // }
+                header("Location:dashboard.php");
+            }
+            else
+            {
+                //user entered incorrect password
+                $error="😞 invalid email or password";
+            }
+        }
+        else 
+        {       // user entered an email that does not exist in our db 
+                $error="😞 invalid email or password";
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,14 +67,23 @@
             <h1>MEDICARE</h1>
             <h3>Treating our patients with love and care</h3>
         </div>
+<!-- displaying the error messages for email or password,mixed style-->
+            <?php 
+            if(!empty($error)){?>
+            <div class="error">
+                <?php echo $error; ?>
+            </div>
+            <?php }?>
+
+        
         <div id="form_container">
-            <form method="POST">
+            <form method="POST" action="login.php">
                 
                    
                     <label for="userid">Email</label><br>
-                    <input id="userid" type="email" name="userid" title="Please ensure your email contains the company domain '@nthanzi.mw'" placeholder="example@nthanzi.mw" required><br> <!--validation not yet set to regex.pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" might not use it-->
+                    <input id="userid" type="text" name="userid" title="Please ensure your email contains the company domain '@nthanzi.mw'" placeholder="example@nthanzi.mw" required><br> <!--validation not yet set to regex.pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" might not use it-->
                     <label for="passcode">password</label><br>
-                    <input type="password" id="passcode" name="passcode" required minlength="12"><br><br>
+                    <input type="password" id="passcode" name="passcode" required><br><br>
                     <button type="submit" id="submit" name="submit">sign in</button>
                 
             </form>
