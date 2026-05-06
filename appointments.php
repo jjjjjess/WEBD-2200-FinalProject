5052403dbae02653309ecd0025aa1b500bf7e9d5
Jessica Jere <jessicanjere@gmail.com>
2026-05-06 21:32:06 +0200

    still trying to style lol

--- appointments.php
+++ appointments.php
@@ -104,13 +104,15 @@
                                 </td>
                                 </tr>
                         </table>
-                        
+                        <div class="buttons">
                         <button type="submit" value="Confirm Booking">
                             Confirm Booking
                         </button>
                         <button type="reset" value="Clear">
                             Clear
                         </button>
+                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
